347a42e391f8a47e8cd87dbe2fd1efcf3efccbd3
MohanElango-prog <mohanelango001@gmail.com>
2023-04-20 15:55:55 +0530

    customized sort added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Table from './table';
 import data from './dummy';
 
 function App() {
-  const column = ["title", "id", "read"]
+  const column = ["id", "first_name", "email", "ip_address", "date"]
+  const sortCol = ["id", "first_name", "date"]
   return (
-    <Table rowData={data} columns ={column} rowsPerPage={5}/>
+    <Table rowData={data} columns ={column} rowsPerPage={100} sortableColumns ={sortCol}/>
   );
 }
 
--- src/table.js
+++ src/table.js
@@ -1,31 +1,50 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Table({ rowData, columns, rowsPerPage }) {
+function Table({ rowData, columns, rowsPerPage, sortableColumns}) {
   const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
   const [currentPage, setCurrentPage] = useState(1);
 
   const handleSort = (column) => {
-    let direction = 'ascending';
-    if (sortConfig && sortConfig.key === column && sortConfig.direction === 'ascending') {
-      direction = 'descending';
+    if (sortableColumns.includes(column)) { // Check if clicked column is sortable
+      let direction = 'ascending';
+      if (sortConfig && sortConfig.key === column && sortConfig.direction === 'ascending') {
+        direction = 'descending';
+      }
+      setSortConfig({ key: column, direction });
     }
-    setSortConfig({ key: column, direction });
   };
 
-  const sortedData = rowData.sort((a, b) => {
-    if (a[sortConfig.key] < b[sortConfig.key]) {
-      return sortConfig.direction === 'ascending' ? -1 : 1;
-    }
-    if (a[sortConfig.key] > b[sortConfig.key]) {
-      return sortConfig.direction === 'ascending' ? 1 : -1;
-    }
-    return 0;
-  });
+  const sortedData = () => {
+    const startIndex = (currentPage - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    const rowsToShow = rowData.slice(startIndex, endIndex);
+  
+    return rowsToShow.sort((a, b) => {
+      const column = sortConfig.key;
+      let valueA = a[column];
+      let valueB = b[column];
+      
+      // check if column data is a string date
+      if (typeof a[column] === 'string' && isNaN(Date.parse(a[column])) === false) {
+        valueA = new Date(a[column]);
+      }
+      if (typeof b[column] === 'string' && isNaN(Date.parse(b[column])) === false) {
+        valueB = new Date(b[column]);
+      }
+      console.log(valueA, valueB);
+      if (valueA < valueB) {
+        return sortConfig.direction === 'ascending' ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return sortConfig.direction === 'ascending' ? 1 : -1;
+      }
+      return 0;
+    });
+  };
+  
 
-
-
-  const pageCount = Math.ceil(sortedData.length / rowsPerPage);
+  const pageCount = Math.ceil(rowData.length / rowsPerPage);
 
   const handlePageClick = (pageNum) => {
     setCurrentPage(pageNum);
@@ -43,13 +62,10 @@ function Table({ rowData, columns, rowsPerPage }) {
       );
     });
   };
+  
 
   const tdData = () => {
-    const startIndex = (currentPage - 1) * rowsPerPage;
-    const endIndex = startIndex + rowsPerPage;
-    const rowsToShow = sortedData.slice(startIndex, endIndex);
-
-    return rowsToShow.map((item, index) => {
+    return sortedData().map((item, index) => {
       return (
         <tr key={index}>
           {columns.map((v, i) => {
